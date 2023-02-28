6d8889776013229f2602d30f1610eda85bf5b962
HyeonHan Bae <baehyunhan3@gmail.com>
2023-02-28 19:06:06 +0900

    Update variables.h

--- main/main/variables.h
+++ main/main/variables.h
@@ -18,3 +18,27 @@ float moveSpeed = 30;//[%]
 // interrupt time setting
 long nowTime = millis();
 long preTime = nowTime;
+
+kp = 10.
+kd = 0.
+ki = 0.
+
+dt = 0.
+dt_sleep = 0.01
+tolerance = 0.1
+
+start_time = time.time()
+error_prev = 0.
+time_prev = 0.
+
+try:
+    while True:
+        motorDeg = encoderPos * ratio
+
+        error = targetDeg - motorDeg
+        de = error - error_prev
+        di += error*dt
+        dt = time.time() - time_prev
+        control = (kp*error) + (kd*de/dt) + (ki*error*dt)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+        error_prev = error
